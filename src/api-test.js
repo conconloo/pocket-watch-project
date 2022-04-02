3f3a38fa07d95e217ac0127af611b381377b189b
ag0711 <ag0711@tamu.edu>
2022-04-02 16:32:13 -0500

    Got an API reponse with available cities in api-test.js

--- src/api-test.js
+++ src/api-test.js
@@ -1,1 +1,12 @@
 // Testing script for API
+
+let apiToken = 'HNMbwNTeRNlQPstmylKvUkIzqcAEjRKH';
+
+const axios = require('axios').get('https://www.ncdc.noaa.gov/cdo-web/api/v2/locations', {
+    headers: {
+        token: apiToken
+    }
+}).then((response) => {
+    console.log(response.data);
+})
+
