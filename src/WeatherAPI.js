7951fc150e46bb8f0f7a20abdd5b0fac2b71327b
kbz2001 <kbz2001@tamu.edu>
2022-04-04 01:43:20 -0500

    Capability for gathering weather information is mostly functional. (exception is description & displaying to HTML)

--- src/WeatherAPI.js
+++ src/WeatherAPI.js
@@ -1,37 +1,50 @@
+import React, { Component } from "react";
+import axios from "axios";
 
-let APIkey = '207a527a4d31d2de8a7264f13e8ad6a2';
-
-let lat = 30.6280; // test values for now
-let lon = -96.3344; // test values for now
-let unit = 'imperial'; // changes from Kelvin to Fahrenheit
-
-// ideally we should use this function to get a users latitude and longitude
-// *MOBILE*: Not sure if this works in mobile, but it might
-
-function getLocation() {
-    let coordinates;
-    if (navigator.geolocation) {
-        coordinates = navigator.geolocation.getCurrentPosition();
-    } else {
-        console.log("Something went wrong!");
+export default class WeatherAPI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            apiKey: '207a527a4d31d2de8a7264f13e8ad6a2',
+            latitude: -1,
+            longitude: -1,
+            units: 'imperial',
+            requestLink: `Null`,
+            city: "Null",
+            temperature: "Null",
+            weather_description: "Null"
+        };
     }
-    console.log(coordinates.latitude); // prints latitude
-    console.log(coordinates.longitude); // print longitude
+    /* function for obtaining weather information from the weather API */
+    handleCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            }
+        );
+
+        // access the API with the parameters obtained from the geo location API
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=${this.state.apiKey}&units=${this.state.units}`).then((response) => {
+            this.setState({
+                city: response.data.name, // gets the name of the city at the latitude and longitude coordinates provided
+                temperature: response.data.main.temp, // gets the current temperature of the city
+                weather_description: response.data.weather.description // gets the description of the current weather of the city
+
+            });
+        });
+};
+    /* This displays the information in HTML */
+    // render() {
+    //     return (
+    //         <div>
+    //             <button onClick={this.handleCurrentLocation}>Testing Weather API</button>
+    //             <h1>The weather in {this.state.city}</h1>
+    //             <p>The temperature is: {this.state.temperature}</p>
+    //             <p>The weather description is: {this.state.weather_description}</p>
+    //         </div>
+    //     );
+    // }
 }
-
-//getLocation();
-
-// const axios = require('axios').get('*insert link*', {
-//     headers: {
-//         token: apiToken
-//     }
-// }).then((response) => {
-//     console.log(response.data);
-// })
-
-let requestLink = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=${unit}`
-
-const axios = require('axios').get(requestLink)
-    .then((response) => {
-        console.log(response.data);
-    })
