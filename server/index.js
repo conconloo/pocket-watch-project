2ebbe64f847c0dfed3772bb9be9b4c631eb510fa
jmcwhirter1608 <jmcwhirter1608@tamu.edu>
2022-04-04 09:59:31 -0500

    Added Server-Client functionality to WeatherReport.js

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const users = require('./routes/users');
 
+
 app.use('/api/users', users);
 
 app.get('/api', (req, res) => {
     res.send('hello world from express');
 });
 
-app.listen(1234);
+app.listen(4000);
