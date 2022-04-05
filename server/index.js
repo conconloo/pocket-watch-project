5c13baa52e996a9abe13955b4879c13e15e7f122
ag0711 <ag0711@tamu.edu>
2022-04-05 13:19:18 -0500

    Express can now get Weather API data

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const users = require('./routes/users');
-const videos = require('./routes/videos')
-
+const videos = require('./routes/videos');
+const weather = require('./routes/weather');
 
 app.use('/api/users', users);
 
 app.use('/api/videos', videos); // for videos
+
+app.use('/api/weather', weather); // for videos
 
 app.get('/api', (req, res) => {
     res.send('hello world from express');
--- server/routes/weather.js
+++ server/routes/weather.js
@@ -3,27 +3,44 @@ const axios = require('axios');
 const { response } = require('express');
 const router = express.Router();
 
-let apiKey = '' // remove this before pushing
+let apiKey = ''; // remove this before pushing
 
 // function that returns the position of the current device
-function getPosition() {
-    return new Promise((res, rej) => {
-        navigator.geolocation.getCurrentPosition(res, rej);
-    });
-}
+// Might have to be on the front end?
+
+// function getPosition() {
+//     return new Promise((res, rej) => {
+//         navigator.geolocation.getCurrentPosition(res, rej);
+//     });
+// }
 
 async function getWeatherData() {
-    let locationData = await getPosition();
+    // let locationData = await getPosition();
+
+    // temporary coordinates for College Station
+    let latitude = 30.6280;
+    let longitude = -96.3344;   
+
     let units = 'imperial';
     const getWeatherData = axios.create({
-       baseURL: `https://api.openweathermap.org/data/2.5/onecall`,
+       baseURL: 'https://api.openweathermap.org/data/2.5/onecall',
         params: {
-            lat: locationData.coords.latitude,
-            lon: locationData.coords.longitude,
-            units: 'imperial',
+            lat: latitude,
+            lon: longitude,
+            units: units,
             appid: apiKey
         }
     });
-    const response = await getWeatherData.get("");
+    const response = await getWeatherData.get(""); // gets info from Weather API
     console.log(response.data);
+    return response.data; // returns all of the data from the response
 }
+
+router.get('/', async (req, res) => {
+    const data = await getWeatherData();
+    res.json([
+        data
+    ]);
+})
+
+module.exports = router;
