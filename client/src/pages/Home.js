66e2524facfe73bce989b923184c40b95fc2dab3
connie.liu <connie.liu@tamu.edu>
2022-04-04 20:32:35 -0500

    start of SOS features

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,49 @@
 import React from "react";
+import { Component } from "react";
 
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('api/users')
+            .then(res => res.json())
+            .then(users => {
+                this.setState({users : users});
+            });
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Pocket-Watch</h1>
+                <div className="SOS">
+                    <h2>SOS Features</h2>
+                    <a href="tel:8063175356"><button className="Police" title="Dial 911">911</button></a>
+                    <button className="Noonlight">Hold Until Safe</button>
+                </div>
+            </div>
+
+            /*
+            <ul>
+                {
+                    this.state.users.map(user =>(
+                        <li>Username {user.username}, Age: {user.age}</li>
+                    ))
+                }
+            </ul>
+            */
+        )
+    }
+
+}
+
+/*
 const Home = () => {
     return (
         <div>
@@ -7,5 +51,6 @@ const Home = () => {
         </div>
     )
 }
+*/
 
 export default Home;
