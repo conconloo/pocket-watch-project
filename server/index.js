1c611d1d5cd135192d56f2cec279b03bc92a1237
ag0711 <ag0711@tamu.edu>
2022-04-06 09:41:10 -0500

    Added in Heroku support to index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const users = require('./routes/users');
+// const users = require('./routes/users');
 const videos = require('./routes/videos');
 const weather = require('./routes/weather');
+
+const path = require('path');
+const PORT = process.env.PORT || 4000;
+// app.use(express.static(path.join(__dirname, 'public'))); // for heroku
 
 app.use('/api/users', users);
 
@@ -14,4 +18,4 @@ app.get('/api', (req, res) => {
     res.send('hello world from express');
 });
 
-app.listen(4000);
+app.listen(PORT);
