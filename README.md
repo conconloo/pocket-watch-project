ded2ba698a25a447fd020d01c1c5b92dc8369bc0
John McWhirter <jmcwhirter1608@tamu.edu>
2022-04-06 21:37:23 -0500

    Built Client and Made Readme for local running - Ready for Heroku End of Sprint 1

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to run this application locally
+
+cd into server directory
+
+run node index.js
+
+open a browser and search http://localhost:4000
+
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const weather = require('./routes/weather');
 
 const path = require('path');
 const PORT = process.env.PORT || 4000;
-// app.use(express.static(path.join(__dirname, 'public'))); // for heroku
+app.use(express.static(path.join(__dirname, 'public'))); // for heroku
 
 
 app.use('/api/videos', videos); // for videos
