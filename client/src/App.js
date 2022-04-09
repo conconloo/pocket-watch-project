2439db37b76a5f2bedb1bb03230d4b8055f0084e
John McWhirter <jmcwhirter1608@tamu.edu>
2022-04-09 15:14:17 -0500

    A few more design changes, plus name changes for cleaner look

--- client/src/App.js
+++ client/src/App.js
@@ -25,15 +25,15 @@ const App = () => {
       <div className="Navbar">
         <div className='logo'>
           <img src={image1} alt='pocket-watch-logo' onClick={() => setActive("home")}/>
-          <h1>Pocket-Watch</h1>
-        </div>
+          <h1>Pocket Watch</h1>
+        </div> 
         <button className="hamburger" id="hamburger" onClick={handleHamburger}>
           <i className="fa fa-bars"></i>
         </button>
         <div className='links' id="links">
           <button onClick={() => setActive("home")} href="/Home">Home</button>
           <button onClick={() => setActive("weatherreport")} href="/Weather">Weather</button>
-          <button onClick={() => setActive("safetyvideos")} href="/SafetyVideos">Safety Videos</button>
+          <button onClick={() => setActive("safetyvideos")} href="/SafetyVideos">Videos</button>
         </div>
       </div>
       <div className="card">
