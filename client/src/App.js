2cfede7b21d54a143dd9aab0be9a593a31b6eeac
John McWhirter <jmcwhirter1608@tamu.edu>
2022-04-10 13:06:04 -0500

    Completed Page Routing This task turned out to be harder than I expected... App.js no longer uses state to set the active page. Instead, react-router-dom has built in features that can link the navbar links, the components, and the url to the page. This also solves the URL issue and Refreshing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Card from "./Card.js";
-import { useState } from "react";
+import { 
+  useState,
+  useEffect
+} from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 import image1 from './images/logo.png';
 
 
-const App = () => {
-  const [active, setActive] = useState("home");
+
+const App = () => {  
+
   const [hamburgerClicked, sethamburgerClicked] = useState(false);
 
   const handleHamburger = () => {
@@ -22,25 +32,30 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="Navbar">
-        <div className='logo'>
-          <img src={image1} alt='pocket-watch-logo' onClick={() => setActive("home")}/>
-          <h1>Pocket Watch</h1>
-        </div> 
-        <button className="hamburger" id="hamburger" onClick={handleHamburger}>
-          <i className="fa fa-bars"></i>
-        </button>
-        <div className='links' id="links">
-          <button onClick={() => setActive("home")} href="/Home">Home</button>
-          <button onClick={() => setActive("weatherreport")} href="/Weather">Weather</button>
-          <button onClick={() => setActive("safetyvideos")} href="/SafetyVideos">Videos</button>
+      
+      <Router>
+        <div>
+          <div className="Navbar">
+            <div className='logo'>
+              <img src={image1} alt='pocket-watch-logo'/>
+              <h1>Pocket Watch</h1>
+            </div> 
+            <button className="hamburger" id="hamburger" onClick={handleHamburger}>
+              <i className="fa fa-bars"></i>
+            </button>
+            <div className='links' id="links">
+              <Link className="link" to="/">Home</Link>
+              <Link className="link" to="/weather">Weather</Link>
+              <Link className="link" to="/videos">Videos</Link>
+            </div>
+          </div>
+            <Routes>
+              <Route path="/" element={<Card title="home"/>}/>
+              <Route path="/weather" element={<Card title="weatherreport"/>} />
+              <Route path="/videos" element={<Card title="safetyvideos"/>}/>
+            </Routes>
         </div>
-      </div>
-      <div className="card">
-        {active === "home" && <Card title = "home"/>}
-        {active === "weatherreport" && <Card title = "weatherreport"/>}  
-        {active === "safetyvideos" && <Card title = "safetyvideos"/>} 
-      </div>
+      </Router>
     </div>
   );
 }
