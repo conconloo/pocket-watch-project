d6839c5519366febfe0556bafb4507be52aff78b
ag0711 <ag0711@tamu.edu>
2022-04-12 13:19:59 -0500

    Added in a WIP videos cahcing feature, changed places.js

--- server/routes/places.js
+++ server/routes/places.js
@@ -6,12 +6,15 @@ const router = express.Router();
 let apiKey = 'AIzaSyAEZeR4pdli80dwbZNLbly_Da9bG-jk1k0';
 
 async function getPlacesList (req = false) {
+    let latitude;
+    let longitude;
+
     if (req != false) {
-        let latitude = req.query.latitude;
-        let longitude = req.query.longitude;
+        latitude = req.query.latitude;
+        longitude = req.query.longitude;
     } else {
-        let latitude = 30.601389; // College Station Latitude
-        let longitude = -96.314445; // College Station Longitude
+        latitude = 30.601389; // College Station Latitude
+        longitude = -96.314445; // College Station Longitude
     }
     // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=5000&type=police&key=${apiKey}
     
