0c92fdb64b7305c7ee4164c05025f6194148daab
jmcwhirter1608 <jmcwhirter1608@tamu.edu>
2022-04-12 13:33:10 -0500

    Testing Places API and preparing to integrate it with design

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -2,11 +2,32 @@ import React from "react";
 import {Component } from "react";
 
 class Map extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
 
 
-
-
+    getPosition = () => {
+        return new Promise(function (resolve, reject) {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+      }
     
+
+    componentDidMount(){
+        this.getPosition()
+        .then((position) => {
+            fetch('api/places?latitude=' + position.coords.latitude + '&longitude=' + position.coords.longitude)
+                .then(res => res.json())
+                .then()
+        })
+    }
+
+
+
     render(){
         return(
         <div>
