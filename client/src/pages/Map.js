8ad6839badf8b27610647d409f960c9a6a91aaa4
jmcwhirter1608 <jmcwhirter1608@tamu.edu>
2022-04-13 10:09:23 -0500

    Working on Maps Page

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,11 +1,17 @@
 import React from "react";
 import {Component } from "react";
 
+
 class Map extends Component {
     constructor(props){
         super(props);
         this.state = {
+            query: [],
+            places: [{
+                name: '',
+                address: '',
 
+            }]
         }
     }
 
@@ -21,17 +27,31 @@ class Map extends Component {
         this.getPosition()
         .then((position) => {
             fetch('api/places?latitude=' + position.coords.latitude + '&longitude=' + position.coords.longitude)
-                .then(res => res.json())
-                .then()
+                .then((res) => res.json())
+                .then(res => this.setState({query: res}))
         })
     }
 
 
 
-    render(){
+    render( ){
         return(
         <div>
             <h1>Header</h1>
+            {this.state.query.map(place => (
+                <>
+                <p>{place.name}</p>
+                <p>{place.vicinity}</p>
+                <p>{place.rating}</p>
+                <p>{place.place_id}</p>
+                <br></br>
+                </>
+            ))}
+            <iframe 
+            className="testFrame"
+            src="https://www.google.com/maps/embed/v1/place
+                ?key=AIzaSyAEZeR4pdli80dwbZNLbly_Da9bG-jk1k0
+                &q=place_id:ChIJs--MqP1YwokRBwAhjXWIHn8"></iframe>
         </div>
         )
     }
