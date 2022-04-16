55d70a18b46ad3bd5aacdbfb4373bad58b601f8e
kbz2001 <kilezisme@gmail.com>
2022-04-16 14:21:42 -0500

    Added a polyline between destinations.

--- client/src/components/MyGoogleMap.js
+++ client/src/components/MyGoogleMap.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
+import {GoogleMap, LoadScript, Marker, Polyline} from '@react-google-maps/api';
 import logo from '../images/logo-40.png';
 import police from '../images/police-40.png';
 import hospital from '../images/Hospital-40.png';
@@ -10,6 +10,20 @@ const containerStyle = { // dimensions of the map
     width: '100%',
     height: '100%'
 }; // TODO: Eventually put this into index.css instead of putting it in the .js file
+
+const options = {
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    radius: 30000,
+    zIndex: 1
+}
 
 class MyGoogleMap extends Component {
     constructor(props) {
@@ -44,15 +58,24 @@ class MyGoogleMap extends Component {
                     zoom={12} // How zoomed in the map is when it's loaded. This varies between 0-22
                 >
                     { /* Child components, such as markers, info windows, etc. */}
+
+                    {/*Marker for the current location of the user */}
                     <Marker
                         icon={logo}
                         label={{text: "Current Location", fontFamily: 'Verdana, sans-serif', fontSize: '2vh', className: 'marker'}}
                         position={{lat: this.state.lat, lng: this.state.lng}}
                     />
+
+                    {/*Marker for the nearest emergency service*/}
                     <Marker
                         onLoad={console.log(this.props.place_position)} // Used for checking if the coords are in the right place
                         label={{text: this.props.place_name, fontFamily: 'Verdana, sans-serif', fontSize: '2vh', className: 'marker'}}
                         position={this.props.place_position}
+                    />
+                    <Polyline
+                        onLoad={console.log("latitude", this.props.place_position.latitude)}
+                        path={[{lat: this.state.lat, lng: this.state.lng}, this.props.place_position]}
+                        options={options}
                     />
                 </GoogleMap>
             </LoadScript>
