370ffc19910263b0cd580a9b2f5024e743d952e1
ag0711 <ag0711@tamu.edu>
2022-05-02 16:17:06 -0500

    Flipped CSV values

--- server/routes/crimeheatmap.js
+++ server/routes/crimeheatmap.js
@@ -84,58 +84,28 @@ async function determineBounds(data) {
     let polygons = getPolygons();
 
     let point = ({lng: parseFloat(data.lat), lat: parseFloat(data.lng), mag: parseFloat(data.mag)})
-    polygons.forEach(polygon => {
-        let topLeft = polygon.square[0];
-        // console.log("Top Left: ", topLeft);
-        // let bottomRight = polygon.square[2];
-        console.log("Bottom Right: ", bottomRight);
-        if (topLeft.lat >= data.lat && data.lat >= bottomRight.lat) {
-            if (topLeft.lng <= bottomRight.lng && topLeft.lng <= data.lng && data.lng <= bottomRight.lng) {
-                polygon.mag += point.mag;
-            } else if (topLeft.lng > bottomRight.lng && (topLeft.lng <= data.lng || data.lng <= bottomRight.lng)) {
-                polygon.mag += point.mag;
+    for (let i = 0; i < 2; ++i) {
+        let top = polygons[i].square[0].lat;
+        let bottom = polygons[i].square[2].lat;
+        let left = polygons[i].square[0].lng;
+        let right = polygons[i].square[2].lng;
+        
+        console.log("top ", top);
+        console.log("bottom ", bottom);
+        console.log("left ", left);
+        console.log("right ", right);
+
+        for (let j = 0; j < data.length; ++j) {
+            if ((top >= data[j]['lat']) && (data[j]['lat'] >= bottom)) {
+                if (left <= right && left <= data[j]['lng'] && data[j]['lng'] <= right) {
+                    polygons[i].mag += point.mag;
+                } else if (left > right && (left <= data[j]['lng'] || data[j]['lng'] <= right)) {
+                    polygons[i].mag += point.mag;
+                }
             }
         }
-    })
-    console.log("Should come back!: ", polygons);
+    }
     return polygons;
-}
-
-
-async function getParsedData() {
-    let results = getPolygons();
-    let file = '../server/datafiles/NIBRSPublicViewJan-Mar22.csv' // hard coded file & file directory
-    let finalResults;
-
-    fs.createReadStream(file)
-        .pipe(csv({mapValues: ({value}) => parseFloat(value)}))
-        .on('data', (data) => {
-            if(data.lat && data.lng && data.mag) { // check if a latitude and longitude exist
-            
-                let point = ({lng: parseFloat(data.lat), lat: parseFloat(data.lng), mag: parseFloat(data.mag)})
-                results.forEach(polygon => {
-                    let TopLeft = polygon.square[0]
-                    let BottomRight = polygon.square[2]
-
-                    //Something is wrong with this statement
-                    
-                    if(point.lat < TopLeft.lat && point.lng > TopLeft.lng){
-                        if(point.lat > BottomRight.lat && point.lng < BottomRight.lng){
-                            //console.log("Top Left:" + TopLeft.lat + ", " + TopLeft.lng, "Point: "+ point.lat + ", " + point.lng)
-                            //console.log("Bottom Right:" + BottomRight.lat + ", " + BottomRight.lng, "Point: "+ point.lat + ", " + point.lng)
-                            //console.log("point Magnitude: ", point.mag, "Polygon mag: ", polygon.mag)
-                            polygon.mag += point.mag
-                        } 
-                    }
-                })
-            }
-        })
-        .on('end', () => {
-            finalResults = results;
-            console.log("What should go to Front-end", finalResults); // check to see if the results are correct
-            return finalResults;
-    });
-    return finalResults;
 }
 
 router.get('/', async (req, res) => {
